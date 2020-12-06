e500ab1a732ea515774fb2c94102616b081b84b6
Petra Vidnerová <petra.vidnerova@gmail.com>
2020-12-06 20:24:01 +0000

    updated, add new config files, obsolete file deleted

--- examples/eval.sh
+++ examples/eval.sh
@@ -1,5 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0
-I=0
-python evaluate_result.py list-front checkpoint_nsga_haklnv_conv_$I.pkl > eval_$I.log
-python evaluate_result.py --conv True eval-front mnist2d.train mnist2d.test checkpoint_nsga_haklnv_conv_$I.pkl 2>eval_$I.err 1>>eval_$I.log
+python evaluate_result.py list-front checkpoint_nsga_test.pkl 
+python evaluate_result.py eval-front checkpoint_nsga_test.pkl 
 
