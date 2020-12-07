f610cb8de4fe153cd6ec1c9161c4bdb1d53bd454
Petra Vidnerová <petra.vidnerova@gmail.com>
2020-12-07 14:29:45 +0000

    default configuration

--- ananas/config.py
+++ ananas/config.py
@@ -1,19 +1,56 @@
 import yaml 
 from pprint import pprint 
 
+# default configuration
+# values can be rewritten by user defined config file 
+global_config = {
+    'network_type': 'dense',
+    'nsga': 2,
+    'main_alg': {
+        'batch_size': 128,
+        'eval_batch_size': 30,
+        'epochs': 10,
+        'loss': 'categorical_crossentropy',
+        'task_type': 'classification',
+        'final_epochs': 20
+    },
+    'ga': {
+        'pop_size': 20,
+        'n_gen': 20
+    },
+    'network': {
+        'max_layers': 5,
+        'max_layer_size': 500,
+        'min_layer_size': 10,
+        'dropout': [0.0, 0.2, 0.3, 0.4],
+        'activations': ['relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear'],
+        # convolutional part
+        'max_dense_layers': 5,
+        'max_conv_layers': 4,
+        'conv_layer': 0.7,
+        'max_pool_layer': 0.3,
+        'min_pool_size': 2,
+        'max_pool_size': 4,
+        'min_filters': 10,
+        'max_filters': 100,
+        'min_kernel_size': 2,
+        'max_kernel_size': 5
+    },
+    'device': {
+        'device_type': 'CPU',
+        'n_cpus': 10
+    }
+}
 
-global_config = {}
 
 
 def load_config(name):
     """ Loads config from file given by name. 
     Saves it in global Config object in module config. """
-
-    global global_config
     
     with open(name, "r") as f:
-        global_config = yaml.full_load(f)
-
+        loaded_config = yaml.full_load(f)
+    global_config.update(loaded_config)
         
 def print_config():
     """ Just pprint the actual global configuration """
